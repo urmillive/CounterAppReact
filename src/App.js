205e71a682158242508a47117fe1f2144401ced7
urmil404 <urmil8989@gmail.com>
2022-06-05 17:01:08 +0530

    alert count button

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import "./App.css";
 
 const App = () => {
   const [count, setCount] = useState(0);
-
+  const alertCount = () => {
+    alert(count); 
+  };
   return (
     <div className="App">
       <header>
@@ -13,8 +15,13 @@ const App = () => {
         Current value of count is <span id="counter">{count}</span>
       </h2>
       <button onClick={() => setCount(0)}>Reset Counter</button>
-      <button onClick={() => setCount(count+1)}>Increase Counter</button>
-      <button onClick={() => setCount(count-1)}>Decrease Counter</button>
+      <button onClick={() => (count > 10 ? 0 : setCount(count + 1))}>
+        Increase Counter
+      </button>
+      <button onClick={() => (count <= 0 ? "" : setCount(count - 1))}>
+        Decrease Counter
+      </button>
+      <button onClick={alertCount}>alert Count</button>
     </div>
   );
 };
